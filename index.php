975f4e433e85677e4d89865deb37764adb541a23
ghazi imran <ghaziimran2105@gmail.com>
2024-07-02 18:33:35 +0100

    commit

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-
-
-
+echo 'Hello From PHP';
 ?>
 
 <!DOCTYPE html>
@@ -11,13 +9,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>Learn PHP From Scratch</title>
+    <title><?php echo 'Learn PHP from Scratch' ?></title>
 </head>
 
 <body class="bg-gray-100">
     <header class="bg-blue-500 text-white p-4">
         <div class="container mx-auto">
-            <h1 class="text-3xl font-semibold">Learn PHP From Scratch</h1>
+            <h1 class="text-3xl font-semibold">?php echo 'Learn PHP From Scratch'?></h1>
         </div>
     </header>
     <div class="container mx-auto p-4 mt-4">
