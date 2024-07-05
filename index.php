e736df9f183e6d5111f0e63ff310149cf20599b6
ghazi imran <ghaziimran2105@gmail.com>
2024-07-05 16:36:53 +0100

    commit

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $title = 'Learn PHP from scratch';
-$heading = 'Welcome to the Course';
+$heading = 'Welcome to the course';
 $body = 'In this course, you will learn the fundamentals of the PHP language'
 
 
