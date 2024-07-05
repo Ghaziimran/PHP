576b4119e767755e26ba8b4a276f916d55af9e61
ghazi imran <ghaziimran2105@gmail.com>
2024-07-05 17:18:39 +0100

    commit

--- Section2/DataTypes/index.php
+++ Section2/DataTypes/index.php
@@ -1,4 +1,43 @@
 <?php
-$title = 'Learn PHP from scratch';
-$heading = 'Welcome to the course';
-$body = 'In this course, you will learn the fundamentals of the PHP language';
+
+//String
+$name = 'Naruto Uzumaki';
+$name2 = "Sasuke";
+
+var_dump($name);
+echo getType($name2);
+echo '<br />';
+
+//Integer
+$age = 35;
+var_dump($age);
+echo '<br />';
+
+//Float
+$rating = 4.5;
+var_dump($rating);
+echo '<br />';
+
+//Boolean
+$isLoaded = true;
+var_dump($isLoaded);
+echo '<br />';
+
+//Array
+$friends = ['john', 'jack', 'jane'];
+var_dump($friends);
+echo '<br />';
+
+//Object
+$person = new stdClass();
+var_dump($person);
+echo '<br />';
+
+//Null
+$car = null;
+var_dump($car);
+echo '<br />';
+
+//Resourse
+$file = fopen('textfile.txt', 'r');
+var_dump($file);
