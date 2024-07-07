6d53459611e54ffc3665dce67d5adbc6c8012b9f
ghazi imran <ghaziimran2105@gmail.com>
2024-07-07 18:04:46 +0100

    commit

--- Section2/Challenge1/index.php
+++ Section2/Challenge1/index.php
@@ -5,7 +5,7 @@ $body = 'PHP (Hypertext Preprocessor) is a widely used server-side scripting
 language that has revolutionized web development. With its simplicity,
 flexibility, and vast community support, PHP has become the backbone of
 countless dynamic websites and web applications.';
-$pageTitle = 'Brad\'s PHP Blog';
+$pageTitle = 'Brad\'s PHP Blog | ' . $title;
 ?>
 
 <!DOCTYPE html>
@@ -14,7 +14,7 @@ $pageTitle = 'Brad\'s PHP Blog';
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title><?= $pageTitle . ' | ' . $title ?></title>
+    <title><?= $pageTitle ?></title>
 </head>
 
 <body>
