a975ed6cea8469b3d29a3241c0eeb59857b94f21
ghazi imran <ghaziimran2105@gmail.com>
2024-07-07 17:57:02 +0100

    commit

--- Section2/Challenge1/index.php
+++ Section2/Challenge1/index.php
@@ -1,3 +1,18 @@
+<?php
+$title = 'Introduction to PHP';
+$author = 'John Doe';
+$body = 'PHP (Hypertext Preprocessor) is a widely used server-side scripting
+language that has revolutionized web development. With its simplicity,
+flexibility, and vast community support, PHP has become the backbone of
+countless dynamic websites and web applications.';
+$pageTitle = '\Brad\'s PHP Blog';
+
+?>
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
