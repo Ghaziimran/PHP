e84070ebeaf2e3696e9fc90e47162fbc86bc6fc6
ghazi imran <ghaziimran2105@gmail.com>
2024-07-10 16:46:50 +0100

    commit

--- Section3/Arrays/index.php
+++ Section3/Arrays/index.php
@@ -3,6 +3,26 @@
 $names = array('john', 'jack', 'jill');
 $numbers = [1, 2, 3, 4, 5, 6];
 
-echo '<pre>';
-var_dump($names);
-echo '</pre>';
+function inspect($value)
+{
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+}
+
+// print_r($names);
+
+// echo $names[2];
+// echo $numbers[3];
+
+//add element to array
+$numbers[6] = 100;
+$numbers[] = 101;
+
+$numbers[3] = 200;
+
+unset($numbers[3]);
+
+$names = array_values($numbers);
+
+inspect($numbers);
