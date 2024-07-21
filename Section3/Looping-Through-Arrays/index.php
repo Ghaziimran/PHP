721c6104381a8e0341e02798ed424678aeef88e4
ghazi imran <ghaziimran2105@gmail.com>
2024-07-21 17:56:58 +0100

    commit

--- Section3/Looping-Through-Arrays/index.php
+++ Section3/Looping-Through-Arrays/index.php
@@ -41,11 +41,22 @@ $users = [
                 <?php endforeach; ?>
             </ul>
             <h3 class="text-xl font-semibold mb-4">Using a foreach loop with index</h3>
-            <ul class="mb-6"></ul>
+            <ul class="mb-6">
+                <?php foreach ($names as $index => $name) : ?>
+                    <li><?= $index . ' - ' . $name; ?></li>
+                <?php endforeach; ?>
+            </ul>
             <h3 class="text-xl font-semibold mb-4">Using a foreach loop with associative array</h3>
-            <ul class="mb-6"></ul>
+            <ul class="mb-6">
+                <?php foreach ($users as $user) : ?>
+                    <li><?= $user['name'] . ' - ' . $user['email']; ?></li>
+                <?php endforeach; ?>
+
+            </ul>
             <h3 class="text-xl font-semibold mb-4">Getting key names and values from associative array</h3>
-            <ul class="mb-6"></ul>
+            <ul class="mb-6">
+
+            </ul>
         </div>
     </div>
 </body>
